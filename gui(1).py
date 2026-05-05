c398237567965c3b8ac5e0590a610fb1af347501
Abdelrahman Mohamed Emam <152274732+AbdelrahmanMohamed75@users.noreply.github.com>
2026-05-05 17:44:09 +0300

    Add files via upload

---
+++ gui(1).py
@@ -0,0 +1,71 @@
+import streamlit as st
+import numpy as np
+import joblib
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import os
+sc=StandardScaler()
+
+#Loading scaleres,encoder and model
+def load_resources():
+    base_path = os.path.join(os.path.dirname(__file__), 'artifacts')
+    
+    model_path = os.path.join(base_path, 'randomforest_model.pkl')
+    encoder_path = os.path.join(base_path, 'le_merchant_category.pkl')
+    scaler_path = os.path.join(base_path, 'final_scaler.pkl')
+    
+    model = joblib.load(model_path)
+    category_encoder = joblib.load(encoder_path)
+    final_scaler = joblib.load(scaler_path)
+    
+    return model, category_encoder, final_scaler
+
+
+def main():
+    #title and header
+    st.title('Credit Card Fraud Detection App')
+    st.header('Enter transaction details:')
+
+    # Load resources
+    model, category_encoder,final_scaler= load_resources()
+
+    # Take user input
+    input_data = {}
+
+    input_data['Transaction_amount'] = st.number_input('Transaction Amount ($)', min_value=0.0)
+    input_data['trans_hour'] = st.slider('Transaction Hour (0–23)', 0, 23, 12)
+    input_data['Merchant_category'] =st.selectbox('Transaction Category', [
+    'gas_transport','grocery_pos','home','shopping_pos','kids_pets',
+    'shopping_net','entertainment','food_dining','personal_care',
+    'health_fitness','misc_pos','misc_net','grocery_net','travel'
+])
+    input_data['zip'] = st.number_input('Zip Code', min_value=-100, max_value=100000, value=0)
+    
+    
+    # Create a DataFrame with user input
+    input_df = pd.DataFrame([input_data])
+    
+    input_df['Merchant_category'] = category_encoder.transform([input_df['Merchant_category'][0]])
+
+    # 2. Apply log transformation to 'Transaction_amount'
+    input_df['Transaction_amount'] = np.log(input_df['Transaction_amount'][0] + 1)
+
+    # 3. Apply individual scaling to each column
+    input_df= final_scaler.transform(input_df)
+
+    
+    # Prediction
+    if st.button('Predict'):
+        expected_columns = ['Transaction_amount', 'trans_hour', 'Merchant_category', 'zip']
+        prediction = model.predict(input_df)
+        
+        if prediction[0] == 1:
+            st.error('Fraudulent Transaction Detected!')
+            st.header('Don\'t worry, we are here to help you!')
+            st.success('We successfully identified and stopped the suspicious transaction before it was completed — your money is safe.')
+        else:
+            st.success('✅ Legitimate Transaction')
+            st.balloons()
+# Run the app
+if __name__ == '__main__':
+    main()
